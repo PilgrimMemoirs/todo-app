c7c781098f4f7e82372b2f1549e9a225da1bd77a
pilgrimmemoirs <jamieleepilgrim@gmail.com>
2015-03-01 19:12:20 -0600

    added factory

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 angular.module('myTodos', [])
+.factory('todos', [function(){
+	var o = {
+		todos: []
+	};
+	return o;
+}])
 .controller('MainCtrl', [
 '$scope',
-function($scope){
+'todos',
+function($scope, todos){
   $scope.test = 'Hello world!';
-  $scope.todos = [
-	{title: "todo 1", priority: 2},
-	{title: "todo 2", priority: 1},
-	{title: "todo 3", priority: 6},
-	{title: "todo 4", priority: 5},
-	{title: "todo 5", priority: 1}
-];
+  $scope.todos = todos.todos;
+//   $scope.todos = [
+// 	{title: "todo 1", priority: 2},
+// 	{title: "todo 2", priority: 1},
+// 	{title: "todo 3", priority: 6},
+// 	{title: "todo 4", priority: 5},
+// 	{title: "todo 5", priority: 1}
+// ];
 	$scope.addTodo = function(){
 		if(!$scope.title || $scope.title === '') { return; }
 		$scope.todos.push({
